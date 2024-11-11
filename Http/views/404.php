65562224702546d1d79f7c6a71e1e3dd37cecd4f
Patriach Cain <youanybluesky10@gmail.com>
2024-11-11 16:23:00 +0800

    Initialized individual product page

--- Http/views/404.php
+++ Http/views/404.php
@@ -9,7 +9,7 @@
             <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something's missing.</p>
             <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
             <a href="/home" class="inline-flex text-white bg-neutral-800 hover:bg-black focus:ring-4
-            focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</a>
+            focus:outline-none focus:ring-neutral-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-neutral-900 my-4">Back to Homepage</a>
         </div>
     </div>
 </section>
---
+++ Http/views/product/index.php
@@ -0,0 +1,10 @@
+<?php
+require base_path("Http/views/partials/head.php");
+require base_path("Http/views/partials/header.php");
+require base_path("Http/views/partials/nav.php");
+require base_path("Http/views/partials/aside.php");
+?>
+  <h1>Under Development</h1>
+<?php
+require base_path("Http/views/partials/footer.php");
+?>
