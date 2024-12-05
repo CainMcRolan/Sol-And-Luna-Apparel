97e8478230f1849ad19a8f0d02f6fd23128c70a5
Patriach Cain <youanybluesky10@gmail.com>
2024-12-05 19:41:49 +0800

    added add and remove favorites in shopping cart

--- Http/views/cart/index.php
+++ Http/views/cart/index.php
@@ -47,26 +47,30 @@ require base_path("Http/views/partials/aside.php");
                                                 Delivery before <?= date('d M Y', strtotime('+7 days')) ?>
                                             </p>
                                             <div class="flex items-center gap-4">
-                                                <?php if ($item['wishlist'] ?? false) : ?>
-                                                    <button type="submit"
-                                                            class="inline-flex items-center text-sm font-medium text-orange-600 hover:text-orange-700">
-                                                        <svg class="me-1.5 h-5 w-5 fill-current" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                                             viewBox="0 0 24 24">
-                                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                                  d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
-                                                        </svg>
-                                                        Remove from Favorites
-                                                    </button>
-                                                <?php else: ?>
-                                                    <button type="button"
-                                                            class="inline-flex items-center text-sm font-medium text-orange-600 hover:text-orange-700">
-                                                        <svg class="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                                  d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
-                                                        </svg>
-                                                        Add to Favorites
-                                                    </button>
-                                                <?php endif; ?>
+                                                <form action="/wishlist" method="POST" class="inline-flex items-center text-sm font-medium text-orange-600 hover:text-orange-700">
+                                                    <input type="hidden" name="product_id" value="<?= $item['product_id'] ?>">
+                                                    <input type="hidden" name="_method" value="<?= $item['wishlist'] ? 'DELETE' : 'POST' ?>">
+                                                    <?php if ($item['wishlist'] ?? false) : ?>
+                                                        <button type="submit"
+                                                                class="inline-flex items-center text-sm font-medium text-orange-600 hover:text-orange-700">
+                                                            <svg class="me-1.5 h-5 w-5 fill-current" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                                                 viewBox="0 0 24 24">
+                                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                      d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
+                                                            </svg>
+                                                            Remove from Favorites
+                                                        </button>
+                                                    <?php else: ?>
+                                                        <button type="submit"
+                                                                class="inline-flex items-center text-sm font-medium text-orange-600 hover:text-orange-700">
+                                                            <svg class="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                      d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
+                                                            </svg>
+                                                            Add to Favorites
+                                                        </button>
+                                                    <?php endif; ?>
+                                                </form>
 
                                                 <form action="/cart" method="POST" class="inline-flex items-center text-sm font-medium text-red-600 hover:underline dark:text-red-500">
                                                     <input type="hidden" name="_method" value="DELETE">
