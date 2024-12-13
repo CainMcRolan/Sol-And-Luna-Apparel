1399cddc1d878e8d021bf95bf47a0d9c71bf2a24
Patriach Cain <youanybluesky10@gmail.com>
2024-12-13 21:48:27 +0800

    a bit more

--- Http/controllers/orders/index.php
+++ Http/controllers/orders/index.php
@@ -1,9 +1,29 @@
 <?php
 
+use Core\App;
+use Core\Database;
 use Core\Session;
+use Http\models\Products;
 
 $errors = Session::get('errors') ?? [];
 $success = Session::get('success') ?? '';
-$email = old('email');
+
+$db = App::resolve(Database::class);
+
+$orders = $db->query("
+    select * 
+    from orders 
+    where user_id = :user_id
+", [
+    ':user_id' => $_SESSION['user']['user_id']
+])->get();
+
+$product = new Products(0);
+$products = array_slice($product->new_products(),0,3);
+
+$wishlist = [];
+if ($_SESSION['user'] ?? false) {
+    $wishlist = array_column($product->wishlist($_SESSION['user']['user_id']), 'product_id');
+}
 
 require base_path('Http/views/orders/index.php');
