bdb8f750b07508a93fa8e43304a6fdfff35783c7
Patriach Cain <youanybluesky10@gmail.com>
2024-12-16 09:21:57 +0800

    added very basic filter system

--- Http/views/orders/index.php
+++ Http/views/orders/index.php
@@ -22,32 +22,6 @@ require base_path("Http/views/partials/aside.php");
                                 </label>
                                 <button class="rounded-md bg-neutral-800 px-4 py-2 text-sm text-white hover:bg-neutral-700">Search</button>
                             </div>
-                            <div class="flex justify-between gap-4">
-                                <button @click="filterExpanded=!filterExpanded" x-ref="filterButton" type="button"
-                                        class="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-900 text-nowrap hover:text-primary-700 hover:bg-gray-100 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 sm:w-auto dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
-                                    <svg class="h-4 w-4 -ms-0.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-                                              d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"></path>
-                                    </svg>
-                                    Filter By
-                                    <svg class="h-4 w-4 -me-0.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
-                                    </svg>
-                                </button>
-                                <?php require base_path("Http/views/orders/filter.php") ?>
-                                <button @click="sortExpanded=!sortExpanded" x-ref="sortButton" type="button"
-                                        class="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-900 text-nowrap hover:text-primary-700 hover:bg-gray-100 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 sm:w-auto dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
-                                    <svg class="h-4 w-4 -ms-0.5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M7 4v16M7 4l3 3M7 4 4 7m9-3h6l-6 6h6m-6.5 10 3.5-7 3.5 7M14 18h4"/>
-                                    </svg>
-                                    Sort
-                                    <svg class="h-4 w-4 -me-0.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/>
-                                    </svg>
-                                </button>
-                                <?php require base_path("Http/views/orders/sort.php") ?>
-                            </div>
                         </div>
 
                         <!-- Order Cards -->
